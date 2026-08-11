011ea34bf60f967f9b6b60d8734c5dd29dab4815
Ethan Cornfield <ethan.cornfield.it@gmail.com>
2026-08-11 14:14:41 +0100

    feat: mount AppShell in root layout, remove AppHeader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
-import AppHeader from "./components/AppHeader";
+import AppShell from "./components/AppShell";
 import { TenantProvider } from "./components/TenantProvider";
 import "./globals.css";
 
@@ -62,8 +62,10 @@ export default function RootLayout({
         }}
       >
         <TenantProvider>
-          <AppHeader />
-          {children}
+          <div style={{ display: "flex", minHeight: "100vh" }}>
+            <AppShell />
+            <div style={{ flex: 1, minWidth: 0 }}>{children}</div>
+          </div>
         </TenantProvider>
       </body>
     </html>
